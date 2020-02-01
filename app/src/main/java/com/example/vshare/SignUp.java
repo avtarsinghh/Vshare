c2cdb9196900dbedb66ddc8d3d16c50f7f22b4b7
Avtar Singh <avi.avtargill@gmail.com>
2020-02-01 10:19:36 -0500

    Login, Sign Up done

--- app/src/main/java/com/example/vshare/SignUp.java
+++ app/src/main/java/com/example/vshare/SignUp.java
@@ -3,7 +3,6 @@ package com.example.vshare;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,11 +19,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    String email, password;
-    EditText emailET, passwordET;
+    String email, password, name, cellNumber;
+    EditText emailET, passwordET, nameET, cellNumberET;
     Button createUser;
     LinearLayout progressBar;
 
@@ -35,16 +38,23 @@ public class SignUp extends AppCompatActivity {
         emailET = findViewById(R.id.email);
         passwordET = findViewById(R.id.password);
         createUser = findViewById(R.id.createBtn);
+        nameET = findViewById(R.id.fullName);
+        cellNumberET = findViewById(R.id.cellNumber);
         mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.llprogressbar);
-
         createUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 email = emailET.getText().toString();
                 password = passwordET.getText().toString();
-                if (email.equals("") || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                name = nameET.getText().toString();
+                cellNumber = cellNumberET.getText().toString();
+                if (name.equals("") || name.length() < 3) {
+                    nameET.setError("Please enter name!!!");
+                } else if (email.equals("") || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                     emailET.setError("Invalid Email");
+                } else if (cellNumber.length() != 10) {
+                    cellNumberET.setError("Invalid cell number");
                 } else if (password.equals("") || password.length() < 6) {
                     passwordET.setError("Password is too short!!");
                 } else {
@@ -55,10 +65,19 @@ public class SignUp extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     progressBar.setVisibility(View.GONE);
                                     if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                        Toast.makeText(getApplicationContext(), "User Account created successfully",
-                                                Toast.LENGTH_LONG).show();
+                                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                        Map<String, String> map = new HashMap<>();
+                                        map.put("name", name);
+                                        map.put("email", email);
+                                        map.put("cellNumber", cellNumber);
+                                        map.put("role", "user");
+                                        db.collection("users").document(task.getResult().getUser().getUid()).set(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                Toast.makeText(SignUp.this, "Success", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    //    Toast.makeText(getApplicationContext(), "User Account created successfully", Toast.LENGTH_LONG).show();
                                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                         startActivity(intent);
                                     } else {
@@ -67,17 +86,10 @@ public class SignUp extends AppCompatActivity {
                                                 Toast.LENGTH_LONG).show();
                                     }
 
-                                    // ...
                                 }
                             });
                 }
             }
         });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        
     }
 }
